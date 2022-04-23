b64801afe1b6f9ed64c26596438c68e2206ad619
michaelballos <mdb3091207@gmai.com>
2022-04-23 09:34:53 -0500

    Add Logo
    Home

--- pages/home.tsx
+++ pages/home.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import type { NextPage } from 'next'
 import HomeContent from '../components/HomeContent';
 import NavbarContent from '../components/NavbarContent';
+import NavLogo from '../components/NavLogo';
 
 const Home: NextPage = () => {
   return (
     <div>
+    <div
+      style={{
+        display: 'flex',
+        width: '24px',
+        paddingLeft: '15px',
+      }} 
+    >
+      <NavLogo />
+    </div>
       <HomeContent />       
     </div>
   )
